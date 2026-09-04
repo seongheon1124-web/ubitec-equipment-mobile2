1bd739701df63f9ee1bf2b9d5f4960ed11bcdab4
seongheon1124-web <seongheon1124@gmail.com>
2026-09-04 17:52:37 +0900

    Add files via upload

---
+++ ubitec_android_project/app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.ubitec.equipmentledger"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ubitec.equipmentledger"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1"
+    }
+}
